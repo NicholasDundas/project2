9697c2544e3b90f281de286de57292948456a7c0
NicholasDundas <nicholasdundas106@gmail.com>
2024-02-17 13:59:09 -0500

    initial

---
+++ code/thread_worker_types.h
@@ -0,0 +1,22 @@
+#ifndef TW_TYPES_H
+#define TW_TYPES_H
+
+#include <ucontext.h>
+
+typedef unsigned int worker_t;
+
+typedef struct TCB
+{
+    /* add important states in a thread control block */
+    // thread Id
+    // thread status
+    // thread context
+    // thread stack
+    // thread priority
+    // And more ...
+
+    // YOUR CODE HERE
+
+} tcb;
+
+#endif
