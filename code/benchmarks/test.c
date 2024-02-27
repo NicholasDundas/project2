e8952caa370b225f0c72a0a306f4f9d8df7251d7
NicholasDundas <nicholasdundas106@gmail.com>
2024-02-26 19:21:19 -0500

    starvation test

--- code/benchmarks/test.c
+++ code/benchmarks/test.c
@@ -4,24 +4,28 @@
 
 #define DEFAULT_THREAD_NUM 6
 
-int global_counter;
-worker_mutex_t mutex;
+int global_counter, global_counter2;
+worker_mutex_t mutex, mutex2;
 
 void dummy_work_short(void *arg)
 {
 	int i = 0;
 	int j = 0;
 	int n = *((int *)arg);
-
-	for (i = 0; i < 700; i++)
+	printf("Thread %d running short\n", n);
+	for (i = 0; i < 30; i++)
 	{
-		for (j = 0; j < 80000; j++)
+		printf("Thread %d attempting to get mutex\n", n);
+		worker_mutex_lock(&mutex);
+		printf("Thread %d got mutex\n", n);
+		int shared = global_counter;
+		for (j = 0; j < 800000; j++)
 		{
 		}
-		printf("Thread %d running\n", n);
-		worker_yield();
+		global_counter = shared + 1;
+		worker_mutex_unlock(&mutex);
+		printf("Thread %d unlocked mutex\n", n);
 	}
-
 	printf("Thread %d exiting\n", n);
 	worker_exit(NULL);
 }
@@ -31,76 +35,42 @@ void dummy_work_long(void *arg)
 	int i = 0;
 	int j = 0;
 	int n = *((int *)arg);
-
-	for (i = 0; i < 75; i++)
+	printf("Thread %d running short\n", n);
+	for (i = 0; i < 30; i++)
 	{
+		printf("Thread %d attempting to get mutex2\n", n);
+		worker_mutex_lock(&mutex2);
+		printf("Thread %d got mutex2\n", n);
+		int shared = global_counter2;
 		for (j = 0; j < 30000000; j++)
 		{
 		}
-		printf("Thread %d running long\n", n);
+		global_counter2 = shared + 1;
+		worker_mutex_unlock(&mutex2);
+		printf("Thread %d unlocked mutex2\n", n);
 	}
-
 	printf("Thread %d exiting\n", n);
 	worker_exit(NULL);
 }
 
-void mutex_work(void *arg)
-{
-	int i = 0;
-	int j = 0;
-	int n = *((int *)arg);
-
-	for (i = 0; i < 7; i++)
-	{
-		worker_mutex_lock(&mutex);
-		for (j = 0; j < 8000000; j++)
-		{
-			global_counter++;
-		}
-		worker_mutex_unlock(&mutex);
-		printf("Thread %d running\n", n);
-	}
-
-	printf("Thread %d exiting\n", n);
-	worker_exit(NULL);
-}
 
 int main(int argc, char **argv)
 {
-	int thread_num;
-	if (argc == 1)
-	{
-		thread_num = DEFAULT_THREAD_NUM;
-	}
-	else
-	{
-		if (argv[1] < 1)
-		{
-			printf("enter a valid thread number\n");
-			return 0;
-		}
-		else
-		{
-			thread_num = atoi(argv[1]);
-		}
-	}
-
 	printf("Running main thread\n");
 
 	worker_mutex_init(&mutex, NULL);
-
+	worker_mutex_init(&mutex2, NULL);
 	int i = 0;
-
-	int *counter = (int *)malloc(thread_num * sizeof(int));
+	int thread_num = 4;
+	int *counter = malloc(thread_num * sizeof(int));
 	for (i = 0; i < thread_num; i++)
 	{
 		counter[i] = i + 1;
 	}
 
-	worker_t *thread = (worker_t *)malloc(thread_num * sizeof(worker_t));
+	worker_t *thread = malloc(thread_num * sizeof(worker_t));
 
-	for (i = 0; i < thread_num; i++)
-	{
+	for (i = 0; i < thread_num; i++) {
 		printf("Main thread creating worker thread %d\n", counter[i]);
 		// if (i % 2 == 0)
 		// {
@@ -110,21 +80,25 @@ int main(int argc, char **argv)
 		// {
 		// 	worker_create(&thread[i], NULL, &dummy_work_long, &counter[i]);
 		// }
-		worker_create(&thread[i], NULL, &mutex_work, &counter[i]);
+		if(i % 2)
+			worker_create(&thread[i], NULL, &dummy_work_long, &counter[i]);
+		else
+			worker_create(&thread[i], NULL, &dummy_work_short, &counter[i]);
+
 	}
 
-	for (i = 0; i < thread_num; i++)
-	{
+	for (i = 0; i < thread_num; i++) {
 		printf("Main thread waiting on thread %d\n", counter[i]);
 		worker_join(thread[i], NULL);
 	}
 
 	printf("Main thread resume\n");
-	printf("%d total increments\n", global_counter);
+	printf("%d global total increments, expected: 30\n", global_counter);
+	printf("%d global2 total increments, expected: 30\n", global_counter2);
 	free(thread);
 	free(counter);
 	worker_mutex_destroy(&mutex);
-
+	worker_mutex_destroy(&mutex2);
 	printf("Main thread exit\n");
 	return 0;
 }
